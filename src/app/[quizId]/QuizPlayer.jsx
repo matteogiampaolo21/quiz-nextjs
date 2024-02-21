4482d1cf11238fc51d458c4185ed0ca9a31ae224
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2024-02-21 14:17:32 +0700

    quiz question go green or red when answering.

--- src/app/[quizId]/QuizPlayer.jsx
+++ src/app/[quizId]/QuizPlayer.jsx
@@ -1,11 +1,15 @@
 'use client'
 import { useParams } from "next/navigation";
-import {doc, getDoc} from 'firebase/firestore';
+import {count, doc, getDoc} from 'firebase/firestore';
 import { db } from "@/firebase/firebase";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const QuizPlayer = () => {
     const params = useParams();
+    const [quiz,setQuiz] = useState(null);
+    const [counter,setCounter] = useState(0);
+    const [isNextBtnDisalbled,setDisabled] = useState(true)
+
 
     const getQuiz = async () => {
         const docRef = doc(db, "quizzes", params.quizId);
@@ -13,6 +17,7 @@ export const QuizPlayer = () => {
 
         if (docSnap.exists()) {
             console.log("Document data:", docSnap.data());
+            setQuiz(docSnap.data());
         } else {
             // docSnap.data() will be undefined in this case
             console.log("No such document!");
@@ -21,11 +26,65 @@ export const QuizPlayer = () => {
 
     useEffect(() => {
         getQuiz();
-    })
+        
+    },[])
+
+    const checkAnswer = (e) => {
+
+        // for (const btn of document.getElementsByTagName("button")){
+        //     console.log(btn.disabled = false)
+        // }
+        if (counter < quiz.questions.length -1 ) {
+            // setCounter(counter+1);
+            setDisabled(false);
+            e.currentTarget.disabled = true
+
+        }else{
+            console.log("Last question")
+        }
+    }
+
+    const nextQuestion = () => {
+        console.log("fe")
+        for (const btn of document.getElementsByTagName("button")){
+            console.log(btn.disabled = false)
+        }
+        setCounter(counter+1);
+        setDisabled(true)
+    }
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    // const handleWrongAnswer = (e) => {
+    //     checkAnswer(false);
+    //     e.currentTarget.disabled = true;
+    // }
 
     return(
         <section>
-            <h1>Hello</h1>
+            {quiz ?
+                <main className="mt-10">
+                    <h2 className="font-bold text-3xl mb-5">{quiz.title}</h2>
+                    <article className="border-black border-2 w-max p-5 grid grid-cols-2 ">
+                        <p className="text-xl mb-3">{quiz.questions[counter].question}</p>
+                        <p className="text-xl mb-3 text-right">Question {counter+1}</p>
+                        <div className="grid grid-cols-2 gap-1 col-span-2">
+                            <button onClick={(e) => {checkAnswer(e)}} className="border-black border-2 p-2 disabled:bg-green-500 hover:bg-neutral-200">{quiz.questions[counter].correctAnswer}</button>
+                            <button onClick={(e) => {e.currentTarget.disabled = true;}} className="border-black disabled:bg-red-500 border-2 p-2 hover:bg-neutral-200">Incorrect Answer 1</button>
+                            <button onClick={(e) => {e.currentTarget.disabled = true;}} className="border-black disabled:bg-red-500 border-2 p-2 hover:bg-neutral-200">Incorrect Answer 2</button>
+                            <button onClick={(e) => {e.currentTarget.disabled = true;}} className="border-black disabled:bg-red-500 border-2 p-2 hover:bg-neutral-200">Incorrect Answer 3</button>
+                        </div>
+                        <button id="Next-btn" disabled={isNextBtnDisalbled} onClick={nextQuestion} className="border-black disabled:border-neutral-500 disabled:text-neutral-600 disabled:bg-neutral-300 mt-1 w-max col-span-2 ml-auto border-2 p-2 hover:bg-neutral-200">Next Question</button>
+                    </article>
+                </main>
+            :
+                <></>
+            }
         </section>
     )
 }
--- src/app/dash/page.js
+++ src/app/dash/page.js
@@ -3,13 +3,16 @@ import { QuizFinder } from "./QuizFinder";
 const Page = () => {
     return(
         <>
-            <div className="my-5">
+            <div className="my-5 border-black border-2 p-5 w-max">
                 <h1 className="text-3xl">Dash</h1>
                 <p>Create Quizzes and complete them!</p>
 
             </div>
             <div className="flex flex-row gap-5">
-                <QuizMaker />
+                <aside className="border-r-2 border-neutral-300 py-5 pr-5">
+                    <h2 className="text-2xl mb-5">Create Quiz</h2>
+                    <QuizMaker />
+                </aside>
                 <QuizFinder />
             </div>
         </>
