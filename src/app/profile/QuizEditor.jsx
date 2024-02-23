361158c6587f0c890093f8d66cf9a3b5e86c4f68
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2024-02-23 12:07:08 +0700

    quizzes can be edited (including the 3 new possible options)

--- src/app/profile/QuizEditor.jsx
+++ src/app/profile/QuizEditor.jsx
@@ -33,13 +33,19 @@ export const QuizEditor = () => {
     },[])
 
     
-    const handleEditQuestion = (e,index,field) => {
+    const handleEditQuestion = (e,quesIndex,ansIndex,field) => {
         e.preventDefault();
-        let tempArr = {...editedQuiz}; 
-        tempArr.questions[index][field] = e.target.value; 
+        let tempObj = {...editedQuiz}; 
+        if (field == "answer") {
+            // Edit Answers
+            tempObj.questions[quesIndex].answers[ansIndex].text = e.target.value; 
+        }else{
+            // Edit Questions
+            tempObj.questions[quesIndex].question = e.target.value; 
+        }
         
-        setEdit(tempArr);
-        console.log(tempArr)
+        setEdit(tempObj);
+        console.log(tempObj)
     }
     
     const handleEditQuiz = async (e,quizID) => {
@@ -81,8 +87,11 @@ export const QuizEditor = () => {
                     {editedQuiz.questions.map((question,index) => 
                         <div className="flex flex-col gap-3 mb-5" key={index}>
                             <p>Question {index+1}</p>
-                            <input className="p-0 placeholder:text-neutral-600 px-2" placeholder="Question" value={question.question} onChange={(e) => handleEditQuestion(e,index,"question")} type="text" />
-                            <input className="p-0 placeholder:text-neutral-600 px-2" placeholder="Answer 1" value={question.correctAnswer} onChange={(e) => handleEditQuestion(e,index,"correctAnswer")} type="text" />
+                            <input className="p-0 placeholder:text-neutral-600 px-2" placeholder="Question" value={question.question} onChange={(e) => handleEditQuestion(e,index,null,"question")} type="text" />
+                            <input className="p-0 placeholder:text-neutral-600 px-2" placeholder="Correct Answer" value={question.answers[0].text} onChange={(e) => handleEditQuestion(e,index,0,"answer")} type="text" />
+                            <input className="p-0 placeholder:text-neutral-600 px-2" placeholder="Incorrect Answer 2" value={question.answers[1].text} onChange={(e) => handleEditQuestion(e,index,1,"answer")} type="text" />
+                            <input className="p-0 placeholder:text-neutral-600 px-2" placeholder="Incorrect Answer 3" value={question.answers[2].text} onChange={(e) => handleEditQuestion(e,index,2,"answer")} type="text" />
+                            <input className="p-0 placeholder:text-neutral-600 px-2" placeholder="Incorrect Answer 4" value={question.answers[3].text} onChange={(e) => handleEditQuestion(e,index,3,"answer")} type="text" />
                         </div>
                     )}
                     <button onClick={(e) => {handleEditQuiz(e,editedQuiz.id)}} className="border-black border-2 hover:bg-neutral-300">Edit Quiz</button>
